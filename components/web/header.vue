5b22bb859ae79be7f8b9edc702a97352aed0a9e7
pramadha <pramslabs@gmail.com>
2025-11-24 14:12:24 +0700

    Enhanced web header with language switcher

--- components/web/header.vue
+++ components/web/header.vue
@@ -20,12 +20,24 @@
             </div>
           </div>
           <div class="col-lg-5 col-xl-4 col-sm-8 col-md-4 col-7">
-            <div class="d-flex justify-content-end">
-              <nuxt-link :to="{name: 'wishlist'}" class="btn btn-light btn-md d-md-block mr-2" v-if="$auth.loggedIn">
-                <i class="fa fa-heart"></i>
-                <span class="badge badge-danger" v-if="wishlistCount > 0">{{ wishlistCount }}</span>
-              </nuxt-link>
-              <nuxt-link :to="{name: 'cart'}" class="btn search-button btn-md d-md-block ml-4">
+            <div class="d-flex justify-content-end align-items-center">
+              <!-- Language Switcher -->
+              <div class="dropdown mr-3 d-none d-md-block">
+                <button class="btn btn-light btn-sm dropdown-toggle" type="button" id="languageDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fa fa-globe"></i> {{ currentLanguage.name }}
+                </button>
+                <div class="dropdown-menu" aria-labelledby="languageDropdown">
+                  <a class="dropdown-item" href="#" @click.prevent="changeLanguage('id')" :class="{ active: lang === 'id' }">
+                    🇮🇩 Indonesia
+                  </a>
+                  <a class="dropdown-item" href="#" @click.prevent="changeLanguage('en')" :class="{ active: lang === 'en' }">
+                    🇺🇸 English
+                  </a>
+                </div>
+              </div>
+              
+              <!-- Cart -->
+              <nuxt-link :to="{name: 'cart'}" class="btn search-button btn-md d-md-block">
                 <i class="fa fa-shopping-cart"></i> 
                 <span class="ml-2">{{ cartTotal }}</span> | Rp. {{ formatPrice(cartPrice) }}
               </nuxt-link>
@@ -66,6 +78,21 @@
             <li class="nav-item"> <nuxt-link :to="{name: 'faq'}" class="nav-link" data-abc="true"><i class="fa fa-question-circle"></i> FAQ</nuxt-link> </li>
           </ul>
           <ul class="navbar-nav ml-auto">
+            <!-- Language Switcher Mobile -->
+            <li class="nav-item dropdown d-md-none">
+              <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-expanded="false">
+                <i class="fa fa-globe"></i> {{ currentLanguage.name }}
+              </a>
+              <div class="dropdown-menu">
+                <a class="dropdown-item" href="#" @click.prevent="changeLanguage('id')" :class="{ active: lang === 'id' }">
+                  🇮🇩 Indonesia
+                </a>
+                <a class="dropdown-item" href="#" @click.prevent="changeLanguage('en')" :class="{ active: lang === 'en' }">
+                  🇺🇸 English
+                </a>
+              </div>
+            </li>
+            
             <li class="nav-item dropdown" v-if="!$auth.loggedIn">
               <nuxt-link :to="{name: 'customer-login'}" class="nav-link" href="#" role="button" aria-expanded="false"> <i class="fa fa-user-circle"></i>
                 ACCOUNT</nuxt-link>
@@ -78,10 +105,10 @@
                 <nuxt-link :to="{name: 'customer-dashboard'}" class="dropdown-item">
                   <i class="fa fa-tachometer-alt"></i> Dashboard
                 </nuxt-link>
-                <nuxt-link :to="{name: 'customer-orders'}" class="dropdown-item">
+                <nuxt-link :to="{name: 'customer-invoices'}" class="dropdown-item">
                   <i class="fa fa-shopping-bag"></i> Pesanan Saya
                 </nuxt-link>
-                <nuxt-link :to="{name: 'customer-profile'}" class="dropdown-item">
+                <nuxt-link :to="{name: 'customer-dashboard'}" class="dropdown-item">
                   <i class="fa fa-user"></i> Profil Saya
                 </nuxt-link>
                 <div class="dropdown-divider"></div>
@@ -99,27 +126,20 @@
 
 <script>
   export default {
-
     //hook "fetch"
     async fetch() {
-
       //fething sliders on Rest API
       await this.$store.dispatch('web/category/getCategoriesData')
 
       if(this.$auth.loggedIn && this.$auth.strategy.name == 'customer') {
-
         //fething carts on Rest API
         await this.$store.dispatch('web/cart/getCartsData')
         await this.$store.dispatch('web/cart/getCartPrice')
-        
-        //fetching wishlist data
-        await this.$store.dispatch('web/wishlist/getWishlistData')
       }
     },
 
     //computed
     computed: {
-        
       //categories
       categories() {
         return this.$store.state.web.category.categories
@@ -135,9 +155,9 @@
         return this.$store.state.web.cart.carts.length
       },
       
-      //wishlistCount
-      wishlistCount() {
-        return this.$store.state.web.wishlist ? this.$store.state.web.wishlist.items.length : 0
+      //current language
+      currentLanguage() {
+        return this.languages.find(lang => lang.code === this.lang) || this.languages[0]
       }
     },
 
@@ -152,8 +172,8 @@
         
         //available languages
         languages: [
-          { code: 'id', name: 'Indonesia' },
-          { code: 'en', name: 'English' }
+          { code: 'id', name: 'ID' },
+          { code: 'en', name: 'EN' }
         ]
       }
     },
@@ -161,6 +181,8 @@
     //method
     methods: {
       searchData() {
+        if (!this.search.trim()) return;
+        
         this.$router.push({
           name: 'search',
           query: {
@@ -178,6 +200,15 @@
         this.lang = langCode;
         this.$i18n.locale = langCode;
         localStorage.setItem('user-language', langCode);
+        
+        // Show confirmation message
+        this.$swal.fire({
+          icon: 'success',
+          title: 'Bahasa Berubah',
+          text: `Bahasa telah diubah ke ${langCode === 'id' ? 'Indonesia' : 'English'}`,
+          timer: 1500,
+          showConfirmButton: false
+        });
       }
     },
     
@@ -211,5 +242,30 @@
   .dropdown-item i {
     width: 20px;
     margin-right: 5px;
+  }
+  
+  .search-form {
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+  
+  .search-button {
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+  }
+  
+  .logo {
+    font-weight: bold;
+    font-size: 1.2rem;
+    margin-left: 10px;
+  }
+  
+  .brand-wrap {
+    text-decoration: none;
+    color: inherit;
+  }
+  
+  .dropdown-toggle::after {
+    vertical-align: 0.155em;
   }
 </style>
