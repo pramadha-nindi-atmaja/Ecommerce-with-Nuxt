c592235d128c298780c1ca5d0da7a0cf6f5f5827
pramadha <pramslabs@gmail.com>
2025-12-17 13:56:12 +0700

    update categories listing page with modern design

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -2,13 +2,14 @@
   <div class="container-fluid mt-custom">
     <div class="fade-in">
       <div class="row justify-content-center">
-          <div class="col-md-3 mb-3" v-for="category in categories" :key="category.id">
-              <div class="card border-0 rounded shadow-sm">
-                  <div class="card-body text-center">
-                      <nuxt-link :to="{name: 'categories-slug', params: {slug: category.slug}}">
-                        <img :src="category.image" width="100">
-                        <hr>
-                        {{ category.name }}
+          <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6 mb-4" v-for="category in categories" :key="category.id">
+              <div class="card border-0 rounded shadow-sm hover-card">
+                  <div class="card-body text-center p-4">
+                      <nuxt-link :to="{name: 'categories-slug', params: {slug: category.slug}}" class="text-decoration-none">
+                        <div class="mb-3">
+                          <img :src="category.image" class="img-fluid rounded" style="height: 80px; object-fit: contain;">
+                        </div>
+                        <h6 class="font-weight-bold text-dark mb-0">{{ category.name }}</h6>
                       </nuxt-link>
                   </div>
               </div>
